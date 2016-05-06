a29d08a812acd11cb34cb9525e813ad18fe81555
yaoyao1992 <tangzhijie115@sina.com>
2016-05-06 16:09:22 +0800

    123

---
+++ README.md
@@ -0,0 +1,17 @@
+
+使用方法:将douyu.bundle复制到/Users/name/Library/FlashlightPlugins
+
+douyu 房间号
+dy 房间号
+
+进行搜索或者打开包配置index.json
+
+{
+	"zhaoshen":"419311"
+}
+
+这样就能直接 搜索dy zhaoshen
+
+<img src='https://github.com/yaoyao1992/123/master/Screenshot.png' width='100%'/>
+
+develop by 球迷
---
+++ douyu.bundle/plugin.py
@@ -0,0 +1,44 @@
+import urllib
+import json
+import i18n
+
+
+def results(parsed, original_query):
+
+    search_specs = [
+        ["Douyu Tv", "~douyuquery", "http://www.douyu.com/"]
+    ]
+
+    f = file("index.json");
+    dict = json.load(f);
+
+    for name, key, url in search_specs:
+
+        if key in parsed:
+            localizedurl = i18n.localstr(url)
+            
+            for dictKey in dict:
+                if parsed[key]==dictKey:
+                   parsed[key]=dict[dictKey]      
+
+            search_url = localizedurl + urllib.quote_plus(parsed[key].encode('UTF-8'))
+            title = i18n.localstr("Search {0} for '{1}'").format(name, parsed[key].encode('UTF-8'))
+            
+            return {
+                "title": title,
+                "run_args": [search_url],
+                "html": """
+                <script>
+                setTimeout(function() {
+                    window.location = %s
+                }, 200);
+                </script>
+                """ % (json.dumps(search_url)),
+                "webview_user_agent": "Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)",
+                "webview_links_open_in_browser": True
+            }
+
+
+def run(url):
+    import os
+    os.system('open "{0}"'.format(url))
